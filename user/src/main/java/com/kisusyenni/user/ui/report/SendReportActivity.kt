52d8f116778f8e873d1db2647be84229fdd2a0f3
Kisusyenni Venessa <kisusyenni01@gmail.com>
2021-12-10 13:09:40 +0700

    Create send report layout xml

---
+++ user/src/main/java/com/kisusyenni/user/ui/report/SendReportActivity.kt
@@ -0,0 +1,12 @@
+package com.kisusyenni.user.ui.report
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kisusyenni.user.R
+
+class SendReportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_report)
+    }
+}
