759010984726f79eac4cf764911f04833ecb3dfe
Kisusyenni Venessa <kisusyenni01@gmail.com>
2021-12-22 14:03:40 +0700

    Get report list data and implement report detail with bottom sheet fragment dialog

---
+++ admin/src/main/java/com/kisusyenni/orgabin/data/source/local/entity/ReportEntity.kt
@@ -0,0 +1,15 @@
+package com.kisusyenni.orgabin.data.source.local.entity
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class ReportEntity (
+    val id: String,
+    val reportDate: String?,
+    val reportTitle: String?,
+    val reportDetail: String?,
+    val senderName: String?,
+    val senderAddress: String?,
+    val senderContact: String?
+): Parcelable
--- admin/src/main/java/com/kisusyenni/orgabin/ui/report/ReportDetailFragment.kt
+++ admin/src/main/java/com/kisusyenni/orgabin/ui/report/ReportDetailFragment.kt
@@ -1,15 +1,27 @@
 package com.kisusyenni.orgabin.ui.report
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import com.kisusyenni.orgabin.R
+import com.kisusyenni.orgabin.data.source.local.entity.ReportEntity
 import com.kisusyenni.orgabin.databinding.FragmentReportDetailBinding
 
 class ReportDetailFragment : BottomSheetDialogFragment() {
+
+    private lateinit var fragmentReportDetailBinding: FragmentReportDetailBinding
+    private lateinit var tvReportContentDate: TextView
+    private lateinit var tvReportContentTitle: TextView
+    private lateinit var tvReportContentDetail: TextView
+    private lateinit var tvReportSender: TextView
+    private lateinit var tvReportAddress: TextView
+    private lateinit var tvReportContact: TextView
+
+    companion object {
+        var EXTRA_REPORT_DETAIL = "extra_report_detail"
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,11 +29,29 @@ class ReportDetailFragment : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val fragmentReportDetailBinding: FragmentReportDetailBinding = FragmentReportDetailBinding.inflate(layoutInflater, container, false)
+        fragmentReportDetailBinding= FragmentReportDetailBinding.inflate(layoutInflater, container, false)
         return fragmentReportDetailBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        tvReportContentDate = fragmentReportDetailBinding.tvReportContentDate
+        tvReportContentTitle = fragmentReportDetailBinding.tvReportContentTitle
+        tvReportContentDetail = fragmentReportDetailBinding.tvReportContentDetail
+        tvReportSender = fragmentReportDetailBinding.tvReportSender
+        tvReportAddress = fragmentReportDetailBinding.tvReportAddress
+        tvReportContact = fragmentReportDetailBinding.tvReportContact
+
+        if (arguments != null) {
+            val reportDetail = arguments?.getParcelable<ReportEntity>(EXTRA_REPORT_DETAIL)
+            tvReportContentTitle.text = reportDetail?.reportTitle
+            tvReportContentDetail.text = reportDetail?.reportDetail
+            tvReportContentDate.text = reportDetail?.reportDate
+            tvReportSender.text = reportDetail?.senderName
+            tvReportAddress.text = reportDetail?.senderAddress
+            tvReportContact.text = reportDetail?.senderContact
+        }
+
     }
 }
