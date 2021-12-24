c87f7c6582d5b32139b28c2546800eaf373bbe78
Kisusyenni Venessa <kisusyenni01@gmail.com>
2021-12-24 17:53:48 +0700

    Add logout logic

--- admin/src/main/java/com/kisusyenni/orgabin/ui/main/AdminActivity.kt
+++ admin/src/main/java/com/kisusyenni/orgabin/ui/main/AdminActivity.kt
@@ -1,13 +1,17 @@
 package com.kisusyenni.orgabin.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.kisusyenni.orgabin.R
+import com.kisusyenni.orgabin.ui.auth.AuthActivity
 
 class AdminActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,5 +26,19 @@ class AdminActivity : AppCompatActivity() {
         ).build()
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_action_bar, menu)
+        return true
+    }
+
+    fun logOutAction(mi: MenuItem?) {
+        if (mi?.itemId == R.id.log_out) {
+            val intent = Intent(this, AuthActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
